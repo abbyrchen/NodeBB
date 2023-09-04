26bc46cd0f18ffb40b3206ecb28d7cd1f949673e
Abby Chen <archen@andrew.cmu.edu>
2023-09-04 12:18:33 -0400

    Made initial translations before debugging

--- src/groups/cover.js
+++ src/groups/cover.js
@@ -1,80 +1,113 @@
-'use strict';
-
-const path = require('path');
-
-const nconf = require('nconf');
-
-const db = require('../database');
-const image = require('../image');
-const file = require('../file');
-
-module.exports = function (Groups) {
-    const allowedTypes = ['image/png', 'image/jpeg', 'image/bmp'];
-    Groups.updateCoverPosition = async function (groupName, position) {
-        if (!groupName) {
-            throw new Error('[[error:invalid-data]]');
-        }
-        await Groups.setGroupField(groupName, 'cover:position', position);
-    };
-
-    Groups.updateCover = async function (uid, data) {
-        let tempPath = data.file ? data.file.path : '';
-        try {
-            // Position only? That's fine
-            if (!data.imageData && !data.file && data.position) {
-                return await Groups.updateCoverPosition(data.groupName, data.position);
-            }
-            const type = data.file ? data.file.type : image.mimeFromBase64(data.imageData);
-            if (!type || !allowedTypes.includes(type)) {
-                throw new Error('[[error:invalid-image]]');
-            }
-
-            if (!tempPath) {
-                tempPath = await image.writeImageDataToTempFile(data.imageData);
-            }
-
-            const filename = `groupCover-${data.groupName}${path.extname(tempPath)}`;
-            const uploadData = await image.uploadImage(filename, 'files', {
-                path: tempPath,
-                uid: uid,
-                name: 'groupCover',
-            });
-            const { url } = uploadData;
-            await Groups.setGroupField(data.groupName, 'cover:url', url);
-
-            await image.resizeImage({
-                path: tempPath,
-                width: 358,
-            });
-            const thumbUploadData = await image.uploadImage(`groupCoverThumb-${data.groupName}${path.extname(tempPath)}`, 'files', {
-                path: tempPath,
-                uid: uid,
-                name: 'groupCover',
-            });
-            await Groups.setGroupField(data.groupName, 'cover:thumb:url', thumbUploadData.url);
-
-            if (data.position) {
-                await Groups.updateCoverPosition(data.groupName, data.position);
-            }
-
-            return { url: url };
-        } finally {
-            file.delete(tempPath);
-        }
-    };
-
-    Groups.removeCover = async function (data) {
-        const fields = ['cover:url', 'cover:thumb:url'];
-        const values = await Groups.getGroupFields(data.groupName, fields);
-        await Promise.all(fields.map((field) => {
-            if (!values[field] || !values[field].startsWith(`${nconf.get('relative_path')}/assets/uploads/files/`)) {
-                return;
-            }
-            const filename = values[field].split('/').pop();
-            const filePath = path.join(nconf.get('upload_path'), 'files', filename);
-            return file.delete(filePath);
-        }));
-
-        await db.deleteObjectFields(`group:${data.groupName}`, ['cover:url', 'cover:thumb:url', 'cover:position']);
-    };
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
+Object.defineProperty(exports, "__esModule", { value: true });
+// 'use strict';
+const path = require("path");
+// const path = require('path');
+const nconf = require("nconf");
+// const nconf = require('nconf');
+const db = require("../database");
+const image = require("../image");
+const file = require("../file");
+/* interface Groups {
+    updateCover: (uid: string, data: GroupData) => Promise<{ url: string; }>;
+    updateCoverPosition: (groupName: string, position: number) => Promise<void>;
+    setGroupField(groupName: string, field: string, value: any): Promise<void>;
+    getGroupFields(groupName: string, fields: string[]): Promise<{ [key: string]: any }>;
+} */
+function default_1(Groups) {
+    const allowedTypes = ['image/png', 'image/jpeg', 'image/bmp'];
+    Groups.updateCoverPosition = function (groupName, position) {
+        return __awaiter(this, void 0, void 0, function* () {
+            if (!groupName) {
+                throw new Error('[[error:invalid-data]]');
+            }
+            yield Groups.setGroupField(groupName, 'cover:position', position);
+        });
+    };
+    Groups.updateCover = function (uid, data) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let tempPath = data.file ? data.file.path : '';
+            try {
+                // Position only? That's fine
+                if (!data.imageData && !data.file && data.position) {
+                    return yield Groups.updateCoverPosition(data.groupName, data.position);
+                }
+                const type = data.file ? data.file.type : image.mimeFromBase64(data.imageData);
+                if (!type || !allowedTypes.includes(type)) {
+                    throw new Error('[[error:invalid-image]]');
+                }
+                if (!tempPath) {
+                    tempPath = yield image.writeImageDataToTempFile(data.imageData);
+                }
+                const filename = `groupCover-${data.groupName}${path.extname(tempPath)}`;
+                const uploadData = yield image.uploadImage(filename, 'files', {
+                    path: tempPath,
+                    uid: uid,
+                    name: 'groupCover',
+                });
+                const { url } = uploadData;
+                // The next line calls a function in a module that has not been updated to TS yet
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+                yield Groups.setGroupField(data.groupName, 'cover:url', url);
+                yield image.resizeImage({
+                    path: tempPath,
+                    // The next line calls a function in a module that has not been updated to TS yet
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+                    width: 358,
+                });
+                const thumbUploadData = yield image.uploadImage(`groupCoverThumb-${data.groupName}${path.extname(tempPath)}`, 'files', {
+                    // The next line calls a function in a module that has not been updated to TS yet
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+                    path: tempPath,
+                    uid: uid,
+                    name: 'groupCover',
+                });
+                yield Groups.setGroupField(data.groupName, 'cover:thumb:url', thumbUploadData.url);
+                if (data.position) {
+                    yield Groups.updateCoverPosition(data.groupName, data.position);
+                }
+                return { url: url };
+            }
+            finally {
+                file.delete(tempPath);
+            }
+        });
+    };
+    // The next line calls a function in a module that has not been updated to TS yet
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call   
+    Groups.removeCover = function (data) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const fields = ['cover:url', 'cover:thumb:url'];
+            // The next line calls a function in a module that has not been updated to TS yet
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            const values = yield Groups.getGroupFields(data.groupName, fields);
+            yield Promise.all(fields.map((field) => {
+                // The next line calls a function in a module that has not been updated to TS yet
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+                if (!values[field] || !values[field].startsWith(`${nconf.get('relative_path')}/assets/uploads/files/`)) {
+                    return;
+                }
+                // The next line calls a function in a module that has not been updated to TS yet
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+                const filename = values[field].split('/').pop();
+                // The next line calls a function in a module that has not been updated to TS yet
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+                const filePath = path.join(nconf.get('upload_path'), 'files', filename);
+                return file.delete(filePath);
+            }));
+            // The next line calls a function in a module that has not been updated to TS yet
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            yield db.deleteObjectFields(`group:${data.groupName}`, ['cover:url', 'cover:thumb:url', 'cover:position']);
+        });
+    };
+}
+exports.default = default_1;
