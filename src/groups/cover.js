47361162261a6ab0e6cee9dce3c926a437a04575
Abby Chen <archen@andrew.cmu.edu>
2023-09-06 14:56:15 -0400

    Created interface for Groups and GroupData

--- src/groups/cover.js
+++ src/groups/cover.js
@@ -9,20 +9,11 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// 'use strict';
 const path = require("path");
-// const path = require('path');
 const nconf = require("nconf");
-// const nconf = require('nconf');
 const db = require("../database");
 const image = require("../image");
 const file = require("../file");
-/* interface Groups {
-    updateCover: (uid: string, data: GroupData) => Promise<{ url: string; }>;
-    updateCoverPosition: (groupName: string, position: number) => Promise<void>;
-    setGroupField(groupName: string, field: string, value: any): Promise<void>;
-    getGroupFields(groupName: string, fields: string[]): Promise<{ [key: string]: any }>;
-} */
 function default_1(Groups) {
     const allowedTypes = ['image/png', 'image/jpeg', 'image/bmp'];
     Groups.updateCoverPosition = function (groupName, position) {
@@ -39,8 +30,12 @@ function default_1(Groups) {
             try {
                 // Position only? That's fine
                 if (!data.imageData && !data.file && data.position) {
-                    return yield Groups.updateCoverPosition(data.groupName, data.position);
+                    yield Groups.updateCoverPosition(data.groupName, data.position);
+                    return { url: '' }; // Return an empty object
                 }
+                // The next line calls a function in a module that has not been updated to TS yet
+                /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,
+                @typescript-eslint/no-unsafe-call */
                 const type = data.file ? data.file.type : image.mimeFromBase64(data.imageData);
                 if (!type || !allowedTypes.includes(type)) {
                     throw new Error('[[error:invalid-image]]');
@@ -49,28 +44,34 @@ function default_1(Groups) {
                     tempPath = yield image.writeImageDataToTempFile(data.imageData);
                 }
                 const filename = `groupCover-${data.groupName}${path.extname(tempPath)}`;
+                // The next line calls a function in a module that has not been updated to TS yet
+                /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,
+                @typescript-eslint/no-unsafe-call */
                 const uploadData = yield image.uploadImage(filename, 'files', {
                     path: tempPath,
                     uid: uid,
                     name: 'groupCover',
                 });
-                const { url } = uploadData;
                 // The next line calls a function in a module that has not been updated to TS yet
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+                /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,
+                @typescript-eslint/no-unsafe-call */
+                const { url } = uploadData;
                 yield Groups.setGroupField(data.groupName, 'cover:url', url);
                 yield image.resizeImage({
                     path: tempPath,
-                    // The next line calls a function in a module that has not been updated to TS yet
-                    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
                     width: 358,
                 });
+                // The next line calls a function in a module that has not been updated to TS yet
+                /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,
+                @typescript-eslint/no-unsafe-call */
                 const thumbUploadData = yield image.uploadImage(`groupCoverThumb-${data.groupName}${path.extname(tempPath)}`, 'files', {
-                    // The next line calls a function in a module that has not been updated to TS yet
-                    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
                     path: tempPath,
                     uid: uid,
                     name: 'groupCover',
                 });
+                // The next line calls a function in a module that has not been updated to TS yet
+                /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,
+                @typescript-eslint/no-unsafe-call */
                 yield Groups.setGroupField(data.groupName, 'cover:thumb:url', thumbUploadData.url);
                 if (data.position) {
                     yield Groups.updateCoverPosition(data.groupName, data.position);
@@ -83,29 +84,38 @@ function default_1(Groups) {
         });
     };
     // The next line calls a function in a module that has not been updated to TS yet
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call   
+    /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,
+    @typescript-eslint/no-unsafe-call */
     Groups.removeCover = function (data) {
         return __awaiter(this, void 0, void 0, function* () {
-            const fields = ['cover:url', 'cover:thumb:url'];
             // The next line calls a function in a module that has not been updated to TS yet
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,
+            @typescript-eslint/no-unsafe-call */
+            const fields = ['cover:url', 'cover:thumb:url'];
             const values = yield Groups.getGroupFields(data.groupName, fields);
+            // The next line calls a function in a module that has not been updated to TS yet
+            /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,
+            @typescript-eslint/no-unsafe-call */
             yield Promise.all(fields.map((field) => {
                 // The next line calls a function in a module that has not been updated to TS yet
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+                /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,
+                @typescript-eslint/no-unsafe-call */
                 if (!values[field] || !values[field].startsWith(`${nconf.get('relative_path')}/assets/uploads/files/`)) {
                     return;
                 }
                 // The next line calls a function in a module that has not been updated to TS yet
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+                /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,
+                @typescript-eslint/no-unsafe-call */
                 const filename = values[field].split('/').pop();
                 // The next line calls a function in a module that has not been updated to TS yet
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+                /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,
+                @typescript-eslint/no-unsafe-call */
                 const filePath = path.join(nconf.get('upload_path'), 'files', filename);
                 return file.delete(filePath);
             }));
             // The next line calls a function in a module that has not been updated to TS yet
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,
+            @typescript-eslint/no-unsafe-call */
             yield db.deleteObjectFields(`group:${data.groupName}`, ['cover:url', 'cover:thumb:url', 'cover:position']);
         });
     };
--- src/groups/cover.ts
+++ src/groups/cover.ts
@@ -1,16 +1,10 @@
-// 'use strict';
 import path = require('path');
-// const path = require('path');
 
 import nconf = require('nconf');
-// const nconf = require('nconf');
 
 import db = require('../database');
 import image = require('../image');
 import file = require('../file');
-/* const db = require('../database');
-const image = require('../image');
-const file = require('../file');*/
 
 interface GroupData {
     groupName: string;
@@ -22,16 +16,24 @@ interface GroupData {
     }
 }
 
-/* interface Groups {
-    updateCover: (uid: string, data: GroupData) => Promise<{ url: string; }>;
-    updateCoverPosition: (groupName: string, position: number) => Promise<void>;
-    setGroupField(groupName: string, field: string, value: any): Promise<void>;
-    getGroupFields(groupName: string, fields: string[]): Promise<{ [key: string]: any }>;
-} */
+interface Data {
+    path: string;
+    uid: string;
+    name: string;
+    url?: string;
+}
 
+interface Groups {
+    updateCoverPosition(groupName: string, position: number): Promise<void>;
+    updateCover(uid: string, data: GroupData): Promise<{ url: string }>;
+    removeCover(data: { groupName: string }): Promise<void>;
+    setGroupField(groupName: string, field: string, value: number): Promise<void>;
+    getGroupFields(groupName: string, fields: string[]): Promise<{ [key: string]: string }>;
+}
 
-export default function(Groups: any): void {
+export default function (Groups: Groups) {
     const allowedTypes = ['image/png', 'image/jpeg', 'image/bmp'];
+
     Groups.updateCoverPosition = async function (groupName: string, position: number): Promise<void> {
         if (!groupName) {
             throw new Error('[[error:invalid-data]]');
@@ -39,14 +41,18 @@ export default function(Groups: any): void {
         await Groups.setGroupField(groupName, 'cover:position', position);
     };
 
-    Groups.updateCover = async function (uid: string, data: GroupData): Promise<{ url: string}> {
+    Groups.updateCover = async function (uid: string, data: GroupData): Promise<{ url: string }> {
         let tempPath = data.file ? data.file.path : '';
         try {
             // Position only? That's fine
             if (!data.imageData && !data.file && data.position) {
-                return await Groups.updateCoverPosition(data.groupName, data.position);
+                await Groups.updateCoverPosition(data.groupName, data.position);
+                return { url: '' }; // Return an empty object
             }
-            const type = data.file ? data.file.type : image.mimeFromBase64(data.imageData);
+            // The next line calls a function in a module that has not been updated to TS yet
+            /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,
+            @typescript-eslint/no-unsafe-call */
+            const type: string = data.file ? data.file.type : image.mimeFromBase64(data.imageData);
             if (!type || !allowedTypes.includes(type)) {
                 throw new Error('[[error:invalid-image]]');
             }
@@ -56,35 +62,42 @@ export default function(Groups: any): void {
             }
 
             const filename = `groupCover-${data.groupName}${path.extname(tempPath)}`;
-            const uploadData = await image.uploadImage(filename, 'files', {
+
+            // The next line calls a function in a module that has not been updated to TS yet
+            /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,
+            @typescript-eslint/no-unsafe-call */
+            const uploadData: Data = await image.uploadImage(filename, 'files', {
                 path: tempPath,
                 uid: uid,
                 name: 'groupCover',
             });
-            const { url } = uploadData;
             // The next line calls a function in a module that has not been updated to TS yet
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,
+            @typescript-eslint/no-unsafe-call */
+            const { url } = uploadData;
             await Groups.setGroupField(data.groupName, 'cover:url', url);
 
             await image.resizeImage({
                 path: tempPath,
-                // The next line calls a function in a module that has not been updated to TS yet
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
                 width: 358,
             });
+
+            // The next line calls a function in a module that has not been updated to TS yet
+            /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,
+            @typescript-eslint/no-unsafe-call */
             const thumbUploadData = await image.uploadImage(`groupCoverThumb-${data.groupName}${path.extname(tempPath)}`, 'files', {
-                // The next line calls a function in a module that has not been updated to TS yet
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
                 path: tempPath,
                 uid: uid,
                 name: 'groupCover',
             });
+            // The next line calls a function in a module that has not been updated to TS yet
+            /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,
+            @typescript-eslint/no-unsafe-call */
             await Groups.setGroupField(data.groupName, 'cover:thumb:url', thumbUploadData.url);
 
             if (data.position) {
                 await Groups.updateCoverPosition(data.groupName, data.position);
             }
-
             return { url: url };
         } finally {
             file.delete(tempPath);
@@ -92,29 +105,38 @@ export default function(Groups: any): void {
     };
 
     // The next line calls a function in a module that has not been updated to TS yet
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call   
+    /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,
+    @typescript-eslint/no-unsafe-call */
     Groups.removeCover = async function (data: { groupName: string }): Promise<void> {
-        const fields = ['cover:url', 'cover:thumb:url'];
         // The next line calls a function in a module that has not been updated to TS yet
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,
+        @typescript-eslint/no-unsafe-call */
+        const fields = ['cover:url', 'cover:thumb:url'];
         const values = await Groups.getGroupFields(data.groupName, fields);
+        // The next line calls a function in a module that has not been updated to TS yet
+        /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,
+        @typescript-eslint/no-unsafe-call */
         await Promise.all(fields.map((field) => {
             // The next line calls a function in a module that has not been updated to TS yet
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,
+            @typescript-eslint/no-unsafe-call */
             if (!values[field] || !values[field].startsWith(`${nconf.get('relative_path')}/assets/uploads/files/`)) {
                 return;
             }
             // The next line calls a function in a module that has not been updated to TS yet
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,
+            @typescript-eslint/no-unsafe-call */
             const filename = values[field].split('/').pop();
             // The next line calls a function in a module that has not been updated to TS yet
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,
+            @typescript-eslint/no-unsafe-call */
             const filePath = path.join(nconf.get('upload_path'), 'files', filename);
             return file.delete(filePath);
         }));
 
         // The next line calls a function in a module that has not been updated to TS yet
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,
+        @typescript-eslint/no-unsafe-call */
         await db.deleteObjectFields(`group:${data.groupName}`, ['cover:url', 'cover:thumb:url', 'cover:position']);
     };
 }
